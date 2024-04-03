3632885af23980f52596f9b2352662a30cacf781
Kwuin <ljkwuin@gmail.com>
2024-04-02 21:10:35 -0400

    shake

--- app/src/main/java/com/example/boggle/MainActivity.kt
+++ app/src/main/java/com/example/boggle/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.example.boggle
 
+import android.content.Context
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.GridLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import java.util.Objects
+import kotlin.math.sqrt
 import kotlin.random.Random
 
 
@@ -12,12 +23,46 @@ class MainActivity : AppCompatActivity() ,TabletFragment.NewGameStarter, GridFra
     private val clickedPositions = mutableListOf<Pair<Int, Int>>()
     private val clickedLetters = mutableListOf<String>()
     //private lateinit var gridLayout: GridLayout
-
+    private var sensorManager: SensorManager? = null
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //newGrid()
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        Objects.requireNonNull(sensorManager)!!
+            .registerListener(sensorListener, sensorManager!!
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+
+        acceleration = 10f
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
+
     }
+
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+
+
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+
+            if (acceleration > 11) {
+                newGame()
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+
 
 
 
